55b475afd9d6d5e4cb0c9598455ca93b3113d597
im281 <iman.mohtashemi@gmail.com>
2016-10-10 14:53:37 -0700

    Lesson 5 GJ elimination

---
+++ M files/Lessson 5/GaussJordan.m
@@ -0,0 +1,22 @@
+function [a] = GaussJordan(a)
+% Gauss-Jordan method
+[m,n]=size(a);
+for j=1:m-1
+    for z=2:m
+        if a(j,j)==0
+            t=a(1,:);a(1,:)=a(z,:);
+            a(z,:)=t;
+        end
+    end
+    for i=j+1:m
+        a(i,:)=a(i,:)-a(j,:)*(a(i,j)/a(j,j));
+    end
+end
+
+for j=m:-1:2
+    for i=j-1:-1:1
+        a(i,:)=a(i,:)-a(j,:)*(a(i,j)/a(j,j));
+    end
+end
+a;
+end 
